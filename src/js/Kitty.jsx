667cacdc24c37459b923ed78b256444224d9a0f8
Max Kalhama <kalhama.max@gmail.com>
2020-11-15 20:45:48 +0200

    Change kitty to be shown only randomly

--- src/js/Kitty.jsx
+++ src/js/Kitty.jsx
@@ -1,5 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export function Kitty() {
-    return <div id="kitty"></div>
+    // Show kitty randomily. Calculate if we should show the kitty every 60s
+
+    const probability = 0.1
+    const [visible, setVisible] = useState(Math.random() + probability >= 1)
+
+    useEffect(() => {
+        const timer = setInterval(() => setVisible(Math.random() + probability >= 1), 60 * 1000)
+        return function cleanup() {
+            clearInterval(timer)
+        }
+    })
+
+    if (visible) return <div id="kitty"></div>
+    else return null
 }
