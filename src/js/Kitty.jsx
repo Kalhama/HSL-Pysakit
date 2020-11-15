a5a75aff93292f0b05b575e31486896c19186dc0
Max Kalhama <kalhama.max@gmail.com>
2020-11-15 23:32:39 +0200

    Simplify kitty randomness and fix memory leak

--- src/js/Kitty.jsx
+++ src/js/Kitty.jsx
@@ -4,14 +4,14 @@ export function Kitty() {
     // Show kitty randomily. Calculate if we should show the kitty every 60s
 
     const probability = 0.1
-    const [visible, setVisible] = useState(Math.random() + probability >= 1)
+    const [visible, setVisible] = useState(Math.random() < probability)
 
     useEffect(() => {
-        const timer = setInterval(() => setVisible(Math.random() + probability >= 1), 60 * 1000)
+        const timer = setInterval(() => setVisible(Math.random() < probability), 60 * 1000)
         return function cleanup() {
             clearInterval(timer)
         }
-    })
+    }, [])
 
     if (visible) return <div id="kitty"></div>
     else return null
